fd3b90a6f8f28b7a956c6d14a6e527ad4b27eb04
zhenpan <astropz@gmail.com>
2018-08-28 19:24:14 -0400

    smp correction

--- sampling.jl
+++ sampling.jl
@@ -4,7 +4,7 @@ function Samp(N_smp::Int, E_σ::Float64, q_min::Float64, q_max::Float64)
     U_uni  = rand(N_smp)
     E3_smp = zeros(N_smp)
     for i = 1:N_smp
-        E3_smp[i] = erfinv(U_uni[i]) * E_σ
+        E3_smp[i] = (erfinv(U_uni[i]))^2 * E_σ
     end
 
     U_uni  = rand(N_smp)
